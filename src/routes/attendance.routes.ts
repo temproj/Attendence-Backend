44786bf8ee292163d9be0032c92531954ee745c1
temproj <23104134010@gecbanka.org>
2025-12-06 10:52:16 +0530

    Update attendance.routes.ts

--- src/routes/attendance.routes.ts
+++ src/routes/attendance.routes.ts
@@ -1,17 +1,21 @@
 // Path: src/routes/attendance.routes.ts
 import { Router } from "express";
 import authMiddleware, { authorize } from "../middlewares/authMiddleware";
+import verifySecretHeader from "../middlewares/secretHeader.middleware";
 import { biometricUpload } from "../middlewares/upload.middleware";
 import { uploadBiometricAttendance } from "../controllers/attendance.controller";
 
-const router = Router();
+const attendanceRoutes = Router();
 
-router.post(
+// Sirf tumhara frontend / cron access kare
+attendanceRoutes.use(verifySecretHeader);
+
+attendanceRoutes.post(
   "/upload/biometric",
   authMiddleware,
-  authorize(["ADMIN", "DEVELOPER"]), // chahe to HOD add
+  authorize(["ADMIN", "DEVELOPER"]), // chahe to "HOD" add kar sakta hai
   biometricUpload.single("file"), // accept .dat/.txt biometric file
   uploadBiometricAttendance
 );
 
-export default router;
+export default attendanceRoutes;
